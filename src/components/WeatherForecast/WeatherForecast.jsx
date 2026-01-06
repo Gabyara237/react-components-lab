a9444aed9aade900186cf25896d31a7ae66e8bb1
Gabriela Araujo <gabyara237@gmail.com>
2026-01-06 15:12:51 -0800

    feat: use WeatherIcon in WeatherForecast component

--- src/components/WeatherForecast/WeatherForecast.jsx
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -1,4 +1,5 @@
 import './WeatherForecast.css'
+import WeatherIcon from '../WeatherIcon/WeatherIcon'
 
 const WeatherForecast = ({weatherForecasts}) =>{
 
@@ -7,7 +8,7 @@ const WeatherForecast = ({weatherForecasts}) =>{
             {weatherForecasts.map((weatherForecast, index)=>(
             <div className="weather" key={index} >
                 <h2> {weatherForecast.day}</h2>
-                <img src={weatherForecast.img} alt={weatherForecast.imgAlt} />
+                <WeatherIcon img={weatherForecast.img} imgAlt={weatherForecast.imgAlt} />
                 <p><span>Conditions: </span> {weatherForecast.conditions} </p>
                 <p><span>Time: </span>{weatherForecast.time}</p>
             </div>
